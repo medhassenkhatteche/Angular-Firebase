8c6cf2db0d43ab0c83be5d311c6730162b21f55a
Mohamed Hassen Khatteche <63561361+medhassenkhatteche@users.noreply.github.com>
2020-05-02 10:20:12 +0100

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "---",
-    authDomain: "---",
-    databaseURL: "---",
-    projectId: "---",
-    storageBucket: "---",
-    messagingSenderId: "---",
-    appId: "---"
+    apiKey: "AIzaSyC-Si7JpueYXLFVmKXDjGZO-b8IT-HvUxM",
+    authDomain: "projet-esthetique.firebaseapp.com",
+    databaseURL: "https://projet-esthetique.firebaseio.com",
+    projectId: "projet-esthetique",
+    storageBucket: "projet-esthetique.appspot.com",
+    messagingSenderId: "327496450484",
+    appId: "1:327496450484:web:bd6737ff8314dbfe8bb2e1"
   }
 };
